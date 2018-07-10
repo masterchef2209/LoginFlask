66769083b450d5342681e202477b83c2366b90bc
Hidayat Khan <huk2209@gmail.com>
2018-07-10 13:48:29 +0530

    checkpoint 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
 app=Flask(__name__)
 
 @app.route('/')
@@ -9,5 +9,16 @@ def home():
 def welcome():
 	return render_template('welcome.html')
 
+@app.route('/login',methods=['GET','POST'])
+def login():	
+	error=None
+	if request.method=='POST':
+		if request.form['username']!='admin' or request.form['password']!='admin':
+			error='Wrong Credentials. Access Denied!'
+		else:
+			return redirect(url_for('home'))
+	return render_template('login.html',error=error)
+
 if __name__=='__main__':
 	app.run(debug=True)
+
