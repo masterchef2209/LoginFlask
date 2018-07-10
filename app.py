37f5731744c85c7f483315640e13056fbccb8b1d
Hidayat Khan <huk2209@gmail.com>
2018-07-10 22:19:22 +0530

    heroku deploying

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask,render_template,redirect,request,url_for,session
 app=Flask(__name__)
 
 @app.route('/')
@@ -16,8 +16,14 @@ def login():
 		if request.form['username']!='admin' or request.form['password']!='admin':
 			error='Wrong Credentials. Access Denied!'
 		else:
+			session['logged_in']=True
 			return redirect(url_for('home'))
 	return render_template('login.html',error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in'.None)
+	return redirect(url_for('welcome'))
 
 if __name__=='__main__':
 	app.run(debug=True)
